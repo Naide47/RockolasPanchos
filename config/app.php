7fee7af165c573423af073649bd783e87829a39b
Lerne70 <56684878+Lerne70@users.noreply.github.com>
2021-07-26 17:26:56 -0500

    ventas v1

--- config/app.php
+++ config/app.php
@@ -176,6 +176,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class
 
     ],
 
@@ -229,6 +230,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'HTML' => Collective\Html\HtmlFacade::class
 
     ],
 
---
+++ database/migrations/2021_07_22_043212_create_usuario.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuario extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->id();
+            $table->string('corrreoElectronico', 100);
+            $table->string('contrasenia', 100);
+            $table->date('fechaRegistro', 100);
+            $table->integer('rol');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
