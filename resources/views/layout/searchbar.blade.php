46db7cd352ef30931ea6b1486030d1c717b98e64
Victor Santillan <victor.santillan.martinez.a@gmail.com>
2021-07-30 18:30:51 -0500

    Usuarios remodelado

--- resources/views/layout/searchbar.blade.php
+++ resources/views/layout/searchbar.blade.php
@@ -1,8 +1,9 @@
 <form>
     <div class="input-group mb-3">
-        <input name="searchBar" id="searchBar" type="text" class="form-control align-self-center" placeholder="Buscar...">
+        <input name="searchBar" id="searchBar" onkeyup="filterTable();" type="text" class="form-control align-self-center"
+            placeholder="Buscar...">
         <div class="input-group-append">
-            <button name="btnReset" id="btnReset" class="btn" type="reset">Reiniciar</button>
+            <button name="btnReset" onclick="clearSearch()" id="btnReset" class="btn" type="reset">Reiniciar</button>
         </div>
     </div>
 </form>
