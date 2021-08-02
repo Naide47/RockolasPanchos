c176892c139a3d289b6f7fee7178cacd3213aaa4
Victor Santillan <victor.santillan.martinez.a@gmail.com>
2021-08-02 10:52:13 -0500

    Remodelacion del modulo Categorias

--- resources/views/categorias/edit.blade.php
+++ resources/views/categorias/edit.blade.php
@@ -1,9 +1,45 @@
 @extends('layout.layout')
-@section('titulo')
+{{-- @section('titulo')
     Editar categoria
+@endsection --}}
+
+@section('head')
+    <link rel="stylesheet" href="{{ asset('css/buttons.css') }}">
 @endsection
 
 @section('contents')
+    <div class="container-fluid bg-white mb-5">
+        {{-- Titulo --}}
+        <div class="row">
+            <div class="col">
+                <h1>Editar categoria</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col mb-3">
+                {{ Form::model($mCategoria, ['route' => ['categorias.update', $mCategoria->id], 'method' => 'PUT']) }}
+                <div class="row bg-light mb-2 pt-2 rounded">
+                    <div class="col">
+                        <div class="form-group">
+                            {{ Form::label('categoria', 'Categoria') }}
+                            {{ Form::text('categoria', $mCategoria->categoria, ['class' => 'form-control', 'required' => true]) }}
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        {{ Form::submit('Confirmar', ['class' => 'btn btn-primary']) }}
+                        <a class="btn btn-success" href="{{ route('categorias.index') }}" role="button">Cancelar</a>
+                    </div>
+                </div>
+                {!! Form::close() !!}
+            </div>
+        </div>
+
+    </div>
+@endsection
+
+@section('contenidos')
     <div class="container">
         <div class="row">
             <div class="col-12">
@@ -11,11 +47,10 @@
                     <h1>Categorias</h1>
                     <h2>- Editar categoria -</h2>
                 </div>
-                {{ Form::model($categoria, ['route' => ['categorias.update', $categoria->id], 'method' => 'PUT']) }}
-
+                {{ Form::model($mCategoria, ['route' => ['categorias.update', $mCategoria->id], 'method' => 'PUT']) }}
                 <div class="form-group">
                     {{ Form::label('categoria', 'Categoria') }}
-                    {{ Form::text('categoria', $categoria->categoria, ['class' => 'form-control', 'required' => true]) }}
+                    {{ Form::text('categoria', $mCategoria->categoria, ['class' => 'form-control', 'required' => true]) }}
                 </div>
                 <div class="row">
                     <div class="col text-right">
@@ -27,5 +62,4 @@
             </div>
         </div>
     </div>
-
 @endsection
--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -1,35 +1,40 @@
 @extends('layout.layout')
-@section('titulo')
+{{-- @section('titulo')
     Categorias de productos
+@endsection --}}
+
+@section('head')
+    <link rel="stylesheet" href="{{ asset('css/buttons.css') }}">
+    <script src="{{ asset('js/index_functions.js') }}"></script>
 @endsection
 
 @section('contents')
-    <div class="container text-center">
+    <div class="container-fluid bg-white mb-5 text-center">
         {{-- Titulo --}}
-        <div class="row mb-5">
-            <div class="col">
-                <h1>Categorias de productos</h1>
+        <div class="row">
+            <div class="col text-left">
+                <h1>Categorias</h1>
             </div>
         </div>
-        {{-- Barra de busqueda y botón de agregar --}}
-        <div class="row mb-3">
+        {{-- Barra de busqueda y boton de agregar --}}
+        <div class="row justify-content-center mb-3">
             <div class="col-10">
                 @include('layout.searchbar')
             </div>
-            <div class="col">
-                <a name="btnCreate" id="btnCreate" class="btn btn-success" href="{{ route('categorias.create') }}"
-                    role="button">Agregar categoria</a>
+            <div class="col-2">
+                <a name="btnAgregarCategoria" id="btnAgregarCategoria" class="btn btn-success"
+                    href="{{ route('categorias.create') }}" role="button">Agregar categoria</a>
             </div>
         </div>
+        {{-- Notificaciones --}}
         @if (Session::has('message'))
-            <div class="alert alert-{{Session::get('alert-class')}}" role="alert">
-                {{Session::get('message')}}
+            <div class="alert alert-{{ Session::get('alert-class') }}" role="alert">
+                {{ Session::get('message') }}
             </div>
         @endif
-
         {{-- Tabla --}}
         <div class="row">
-            <div class="col">
+            <div class="col bg-light mb-5 rounded pt-5">
                 <table class="table">
                     <caption>Categorias de los productos</caption>
                     <thead>
@@ -54,11 +59,10 @@
                                 <td colspan="3">Sin registros</td>
                             </tr>
                         @endforelse
-
                     </tbody>
                 </table>
             </div>
-
         </div>
     </div>
+
 @endsection
